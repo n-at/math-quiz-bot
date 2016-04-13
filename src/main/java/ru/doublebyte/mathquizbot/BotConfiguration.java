85f641e2c1a1fdd1ccfe6cff80f67ff974990e84
Alexey Nurgaliev <atnurgaliev@gmail.com>
2016-04-13 22:43:05 +0300

    properties

--- src/main/java/ru/doublebyte/mathquizbot/BotConfiguration.java
+++ src/main/java/ru/doublebyte/mathquizbot/BotConfiguration.java
@@ -1,10 +1,15 @@
 package ru.doublebyte.mathquizbot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BotConfiguration {
 
+    @Value("${bot.api_url}")
+    private String apiUrl;
 
+    @Value("${bot.token}")
+    private String token;
 
 }
